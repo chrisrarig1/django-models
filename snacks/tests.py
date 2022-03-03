e6a51c95b6a277e73033f7c830a3df385cecf325
Chris Rarig <cwrarig20@gmail.com>
2022-03-03 00:08:52 -0600

    first commit

---
+++ snacks/tests.py
@@ -0,0 +1,33 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+from snacks.models import Snack
+
+# Create your tests here.
+
+class MoviesTests(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="tester", email="tester@email.com", password="pass")
+        self.snack = Snack.objects.create(
+            name = 'Twix', rating = 10, reviewer = self.user)
+
+    def test_string_representation(self):
+        self.assertEqual(str(self.snack), 'Twix')
+
+    def test_movie_name(self):
+        self.assertEqual(f'{self.snack.name}', 'Twix')
+
+    def test_list_page_status_code(self):
+        url = reverse("snack_list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_list_page_template(self):
+        url = reverse("snack_list")
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, "snack_list.html")
+        self.assertTemplateUsed(response, "base.html")
+
+# Create your tests here.
